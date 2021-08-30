2ab0900700c8a0351b671740e03033185b2c1dcb
Steve Clark <stclark278@gmail.com>
2021-08-30 16:59:35 -0500

    Added Hello World Label and form Title.

---
+++ P1 Code/P1 Code/FormHelloWorld.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace P1_Code
+{
+    partial class FormHelloWorld
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblHelloWorld.Location = new System.Drawing.Point(38, 31);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(271, 55);
+            this.lblHelloWorld.TabIndex = 0;
+            this.lblHelloWorld.Text = "Hello World";
+            this.lblHelloWorld.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 173);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Name = "FormHelloWorld";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CSC 470 - P1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHelloWorld;
+    }
+}
+
--- P1 Code/P1 Code/Program.cs
+++ P1 Code/P1 Code/Program.cs
@@ -17,7 +17,7 @@ namespace P1_Code
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormHelloWorld());
         }
     }
 }
